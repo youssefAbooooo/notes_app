9b355dbb5b71eba1b551d25f77e6e80523046e4d
youssefAbooooo <99736061+youssefAbooooo@users.noreply.github.com>
2025-03-04 00:11:57 +0200

    adding AbsorbPointer widget for the addNoteLoadingState

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -13,22 +13,29 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 32.0,
-        ),
-        child: BlocListener<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteSuccessState) {
-              Navigator.pop(context);
-            }
-            if (state is AddNoteFailureState) {
-              log('failed ${state.errorMessage}');
-            }
-          },
-          child: AddNoteForm(),
-        ),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteSuccessState) {
+            Navigator.pop(context);
+          }
+          if (state is AddNoteFailureState) {
+            log('failed ${state.errorMessage}');
+          }
+        },
+        builder: (context, state) {
+          return AbsorbPointer(
+            absorbing: state is AddNoteLoadingState
+                ? true //* if state is addNoteLoadingState, then don't allow pressing on the screen.
+                : false, //* else, allow pressing on the screen.
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 32.0,
+              ),
+              child: AddNoteForm(),
+            ),
+          );
+        },
       ),
     );
   }
